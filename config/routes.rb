6194b592f2c7ae43f556a75b7ced524f701d68df
Patrick Woods <patrickjwoods@gmail.com>
2013-08-01 21:48:27 -0500

    Finished routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
-  get "pages/contact"
-  get "pages/about"
-  get "pages/help"
+  get "users/new"
+  root 'pages#home'
+  get "/contact"  => 'pages#contact'
+  get "/about"    => 'pages#about'
+  get "/help"     => 'pages#help'
+  get "/signup"   => 'users#new'
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
