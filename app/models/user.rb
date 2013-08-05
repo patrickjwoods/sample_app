2ac22ca4b4d55073a32b599b38f27358cc5f183c
Patrick Woods <patrickjwoods@gmail.com>
2013-08-04 22:08:29 -0500

    User model with passwords, and user show page.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,18 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer          not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#  salt               :string(255)
+#  password           :string(255)
 #
 
 class User < ActiveRecord::Base
-	attr_accessible :name, :email
+	attr_accessible :name, :email, :password, :password_confirmation
 
 	email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -20,5 +23,43 @@ class User < ActiveRecord::Base
 	validates :email, :presence	 	=> true,
 					  :format 		=> { :with => email_regex },
 					  :uniqueness 	=> { :case_sensitive => false }
+
+	# Automatically create the virtual attribute 'password_confirmation'.	
+	validates :password, :presence 		=> true,
+						 :confirmation	=> true,
+						 :length		=> { :within => 6..40 }
+
+	before_save :encrypt_password
+
+	# Return true if the user's password matches the submitted password.
+
+	def has_password?(submited_password)
+		encrypted_password == encrypt(submited_password)
+	end
+
+	def self.authenticate(email, sub)
+		user = find_by_email(email)
+		return nil if user.nil?
+		return user if user_has_password?(submited_password)
+	end
+
+	private #methods below are not available publicly! 
+
+		def encrypt_password
+			self.salt = make_salt if new_record?
+			self.encrypted_password = encrypt(password)
+		end
+
+		def encrypt(string)
+			secure_hash("#{salt}--#{string}")
+		end
+
+		def make_salt
+			secure_hash("#{Time.now.utc}--#{password}")
+		end
+
+		def secure_hash(string)
+			Digest::SHA2.hexdigest(string)
+		end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130804005230) do
+ActiveRecord::Schema.define(version: 20130804223905) do
 
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password"
+    t.string   "salt"
+    t.string   "password"
   end
 
 end
